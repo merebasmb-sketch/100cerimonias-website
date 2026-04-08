108abb839b49dd118a43f374132d20f9024807db
merebasmb-sketch <merebasmb@gmail.com>
2026-04-08 23:37:06 +0000

    Font: Cormorant Garamond -> Playfair Display

--- src/web/pages/index.tsx
+++ src/web/pages/index.tsx
@@ -1773,10 +1773,10 @@ function Index() {
       )}
 
       <style>{`
-        @import url('https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;0,500;0,600;0,700;1,400&family=Montserrat:wght@300;400;500;600&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400&family=Montserrat:wght@300;400;500;600&display=swap');
 
         .font-serif {
-          font-family: 'Cormorant Garamond', Georgia, serif;
+          font-family: 'Playfair Display', Georgia, serif;
         }
 
         .font-sans {
