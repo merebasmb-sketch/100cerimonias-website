91ccc4ff1968226fb7c7bb152c0ea1f32f444161
merebasmb-sketch <merebasmb@gmail.com>
2026-04-16 19:31:34 +0000

    Remove Pinot Noir

--- src/web/pages/index.tsx
+++ src/web/pages/index.tsx
@@ -222,7 +222,6 @@ const menuData = {
         regiao: "Tejo", regiaoEn: "Tejo",
         tipos: [
           { tipo: "Tinto", tipoEn: "Red", vinhosData: [
-            { name: "Quinta da Lapa Pinot Noir Reserva", price: "28" },
             { name: "Quinta da Lapa Reserva Cabernet Sauvignon", price: "28" },
           ]},
         ]
